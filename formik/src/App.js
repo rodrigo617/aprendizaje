28a3fdb5326d3b12c6c075158c32b98a63606953
Rodrigo617 <ingenieria.rlopez@gmail.com>
2022-01-06 16:27:45 -0300

    intro to formik: useFormik hook, initialValues, handleSubmit property, handleChange property

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,7 +1,39 @@
-
+import { useFormik } from 'formik'
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      lastname: '',
+      email: '',
+    },
+    onSubmit: values => console.log(values)
+  })
   return (
+    <form onSubmit={formik.handleSubmit}>
+      <label>Nombre</label>
+      <input
+        name='name'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.name}
+        />
+      <label>Apellido</label>
+      <input
+        name='lastname'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.lastname}
+        />
+      <label>Email</label>
+      <input
+        name='email'
+        type='text'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+        />
+        <button type="submit">Enviar</button>
+    </form>
 
 );
 }
