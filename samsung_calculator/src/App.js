a78e2c2f38af2f79e10c590a3c8197ce231c3a38
Rodrigo617 <ingenieria.rlopez@gmail.com>
2022-01-21 16:36:51 -0300

    I created buttons with styles component

--- samsung_calculator/src/App.js
+++ samsung_calculator/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import styled from "styled-components"
+
+const Button = styled.button`
+  color: white;
+  background-color: grey;
+  font-size: 18px;
+`
+const GreenButton = styled(Button)`
+  color: green;
+`
+const IqualButton = styled(Button)`
+  background-color: green;
+`
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calculator-grid'>
+        <div className='output'>
+          <div className='previus-operant'></div>
+          <div className='current-operent'></div>
+        </div>
+        <div className='tools'></div>
+        <Button>C</Button>
+        <GreenButton>()</GreenButton>
+        <GreenButton>%</GreenButton>
+        <GreenButton>/</GreenButton>
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button>
+        <GreenButton>x</GreenButton>
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
+        <GreenButton>-</GreenButton>
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <GreenButton>+</GreenButton>
+        <Button>+/-</Button>
+        <Button>0</Button>
+        <Button>.</Button>
+        <IqualButton>=</IqualButton>
+      </div>
     </div>
   );
 }
