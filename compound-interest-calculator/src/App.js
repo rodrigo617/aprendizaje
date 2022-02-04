b6727d0da18ebc31895b4c1f07c667fb4079cce6
Rodrigo617 <ingenieria.rlopez@gmail.com>
2022-02-04 17:48:50 -0300

    I finished the lab. In it, I used Formik, Styled components and performed validations with Yup

--- compound-interest-calculator/src/App.js
+++ compound-interest-calculator/src/App.js
@@ -1,13 +1,34 @@
-import  styled from 'styled-components'
-import  { Formik, Form } from 'formik'
+import { useState } from 'react'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup' 
 import Input from './components/Input'
 import Button from './components/Button'
 import Container from './components/Container'
 import Section from './components/Section'
+import Balance from './components/Balance'
 
+const compoundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit
+  for (let i = 0; i < years; i++){
+    total = (total + contribution) * (rate +1)
+  }
+
+  return Math.round(total)
+}
+
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
 
 function App() {
-  const handleSubmit = () => {}
+  const [balance, setBalanace] = useState('')
+  const handleSubmit = ({deposit, contribution, years, rate}) => {
+    const val = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    setBalanace(formatter.format(val))
+  }
 
   return (
     <Container>
@@ -20,18 +41,29 @@ function App() {
             rate: '',
           }}
           onSubmit={handleSubmit}
+          validationSchema={Yup.object({
+            deposit: Yup.number().required('Obligatorio').typeError('Debe ser un número'),
+            contribution: Yup.number().required('Obligatorio').typeError('Debe ser un número'),
+            years: Yup.number().required('Obligatorio').typeError('Debe ser un número'),
+            rate: Yup.
+              number()
+              .required('Obligatorio')
+              .typeError('Debe ser un número')
+              .min(0, 'El valor mínimo es 0')
+              .max(1, 'El valor máximo es 1'),
+          })}
         >
           <Form>
             <Input name="deposit" label="Deposito inicial" />
             <Input name="contribution" label="Contribución inicial" />
             <Input name="years" label="Años" />
             <Input name="rate" label="Interés estimado" />
-            <Button>Calcular</Button>
+            <Button type='submit'>Calcular</Button>
           </Form>
         </Formik>
+        {balance !== '' ? <Balance>Balanace final: {balance}</Balance> : null}
       </Section>
     </Container>
   )
 }
-
 export default App
