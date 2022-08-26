8f306ea6487a4f8783d93a90f278d0797211afae
rodrigoWSL <ingenieria.rlopez@gmail.com>
2022-08-25 23:44:05 -0300

    first steps in codeCademy

---
+++ javaScript/dog-year/dog-year.js
@@ -0,0 +1,17 @@
+let myAge =31 // edad humana
+
+let earlyYears = 2
+
+earlyYears *= 10.5
+
+let laterYears = myAge - 2
+
+laterYears *= 4 
+
+let myAgeInDogYears = laterYears + earlyYears //edad en perros
+
+myName = 'Rodrigo'.toLowerCase()
+
+
+console.log (`My name is ${myName}. I am ${myAgeInDogYears} years ol in dog years`)
+
---
+++ javaScript/kelvin-weather/kelvin-weather.js
@@ -0,0 +1,14 @@
+let kelvin = 293; //constante
+
+let celsius = kelvin - 293; //conversor
+
+let fahrenheit = celsius * (9 / 5) + 32; //convierto a F
+fahrenheit = Math.floor(fahrenheit);
+
+console.log("The temperature es " + fahrenheit + " degrees Fahrenheit");
+
+let newton = celsius * (33 / 100)
+
+newton = Math.floor(newton)
+
+console.log (`The temperature is ${newton} degrees Newton`)
